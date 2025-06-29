d1583179d936751861fed9ebb4bac48e9be8af9a
Fred Trotter <fred.trotter@gmail.com>
2025-06-29 12:46:24 -0400

    initial

--- go.sh
+++ go.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 python Step10_extract_list_source_from_lantern_csv.py --input_file prod_data/fhir_endpoints.csv --output_file prod_data/list_sources_summary.csv
+python Step20_download_list_source_json.py --input_file ./prod_data/list_sources_summary.csv --output_dir ./data/service_json/
+python Step30_parse_source_bundle.py --input_dir ./data/service_json/
+
